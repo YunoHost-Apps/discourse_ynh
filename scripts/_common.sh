4fdbfe333c5a0a9d4e96f325b37514ad432355da
Jimmy Monin <jimmy@monin.net>
2018-04-30 18:53:20 +0200

    Check memory requirements to prevent install/upgrade/restore processes from being killed

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -27,6 +27,47 @@ exec_as() {
   fi
 }
 
+# Returns true if a swap partition is enabled, false otherwise
+# usage: is_swap_present
+is_swap_present() {
+  [ $(awk '/^SwapTotal:/{print $2}' /proc/meminfo)  -gt 0 ]
+}
+
+# Returns true if swappiness higher than 50
+# usage: is_swappiness_sufficient
+is_swappiness_sufficient() {
+  [ $(cat /proc/sys/vm/swappiness)  -gt 50 ]
+}
+
+# Returns true if specified free memory is available (RAM + swap)
+# usage: is_memory_available MEMORY (in bytes)
+is_memory_available() {
+  local needed_memory=$1
+  local freemem="$(awk '/^MemAvailable:/{print $2}' /proc/meminfo)"
+  local freeswap="$(awk '/^SwapFree:/{print $2}' /proc/meminfo)"
+  [ $(($freemem+$freeswap)) -gt $needed_memory ]
+}
+
+# Checks discourse install memory requirements
+# terminates installation if requirements not met
+check_memory_requirements() {
+  if ! is_swap_present ; then
+    ynh_die "You must have a swap partition in order to install and use this application"
+  elif ! is_swappiness_sufficient ; then
+    ynh_die "Your swappiness must be higher than 50; please see https://en.wikipedia.org/wiki/Swappiness"
+  elif ! is_memory_available 1000000 ; then
+    ynh_die "You must have a minimum of 1Gb available memory (RAM+swap) for the installation"
+  fi
+}
+# Checks discourse upgrade memory requirements
+# Less requirements as the software is already installed and running
+# terminates upgrade if requirements not met
+check_memory_requirements_upgrade() {
+  if ! is_memory_available 400000 ; then
+    ynh_die "You must have a minimum of 400Mb available memory (RAM+swap) for the upgrade"
+  fi
+}
+
 #=================================================
 # POSTGRES HELPERS
 #=================================================
