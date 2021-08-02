760652774462981ae42f5d83ae3637b0b94b4299
ericgaspar <junk.eg@free.fr>
2021-08-02 14:26:34 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 pkg_dependencies="g++ libjemalloc1|libjemalloc2 libjemalloc-dev zlib1g-dev libreadline-dev libpq-dev libssl-dev libyaml-dev libcurl4-dev libapr1-dev libxslt1-dev libxml2-dev vim imagemagick postgresql postgresql-server-dev-all postgresql-contrib optipng jhead jpegoptim gifsicle brotli"
 
-RUBY_VERSION="3.0.0"
+RUBY_VERSION="2.7.2"
 
 #=================================================
 # PERSONAL HELPERS
