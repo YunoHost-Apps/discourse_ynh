1d564ea1c592d5c1d339466397abe3c438f71299
Félix Piédallu <felix@piedallu.me>
2024-01-26 22:16:29 +0100

    manifestv2

--- doc/DESCRIPTION_fr.md
+++ doc/DESCRIPTION_fr.md
@@ -1,1 +1,7 @@
-Discourse est un logiciel de forum moderne pour votre communauté. Utilisez-le comme liste de diffusion, forum de discussion, salle de discussion longue durée, et plus encore !
+[Discourse](http://www.discourse.org) est la plate-forme de discussion 100% open source conçue pour la prochaine décennie d'Internet. Utilisez-le comme :
+
+- liste de diffusion
+- forum de discussion
+- salle de discussion longue durée
+
+Pour en savoir plus sur la philosophie et les objectifs du projet, [visitez **discourse.org**](http://www.discourse.org).
--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,14 +4,11 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-#pkg_dependencies="g++ libjemalloc1|libjemalloc2 libjemalloc-dev zlib1g-dev libreadline-dev libpq-dev libssl-dev libyaml-dev libcurl4-dev libapr1-dev libxslt1-dev libxml2-dev vim imagemagick postgresql postgresql-server-dev-all postgresql-contrib optipng jhead jpegoptim gifsicle brotli"
-pkg_dependencies="postgresql postgresql-client postgresql-contrib imagemagick libjemalloc1|libjemalloc2"
-build_pkg_dependencies="libcurl4-openssl-dev libyaml-dev libxml2-dev libpq-dev libreadline-dev brotli libunwind-dev libtcmalloc-minimal4 cmake pngcrush pngquant advancecomp jhead jpegoptim libjpeg-turbo-progs optipng"
-
 ruby_version="3.0.0"
 
 nodejs_version="16"
+
+libjemalloc="$(ldconfig -p | grep libjemalloc | awk 'END {print $NF}')"
 
 #=================================================
 # PERSONAL HELPERS
@@ -58,82 +55,82 @@ check_memory_requirements_upgrade() {
   fi
 }
 
-ynh_maintenance_mode_ON () {
-	# Load value of $path_url and $domain from the config if their not set
-	if [ -z $path_url ]; then
-		path_url=$(ynh_app_setting_get $app path)
-	fi
-	if [ -z $domain ]; then
-		domain=$(ynh_app_setting_get $app domain)
-	fi
+# ynh_maintenance_mode_ON () {
+# 	# Load value of $path and $domain from the config if their not set
+# 	if [ -z $path ]; then
+# #REMOVEME? 		path=$(ynh_app_setting_get $app path)
+# 	fi
+# 	if [ -z $domain ]; then
+# #REMOVEME? 		domain=$(ynh_app_setting_get $app domain)
+# 	fi
 
-	# Create an html to serve as maintenance notice
-	echo "<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="refresh" content="3">
-<title>Your app $app is currently under maintenance!</title>
-<style>
-	body {
-		width: 70em;
-		margin: 0 auto;
-	}
-</style>
-</head>
-<body>
-<h1>Your app $app is currently under maintenance!</h1>
-<p>This app has been put under maintenance by your administrator at $(date)</p>
-<p>Please wait until the maintenance operation is done. This page will be reloaded as soon as your app will be back.</p>
+# 	# Create an html to serve as maintenance notice
+# 	echo "<!DOCTYPE html>
+# <html>
+# <head>
+# <meta http-equiv="refresh" content="3">
+# <title>Your app $app is currently under maintenance!</title>
+# <style>
+# 	body {
+# 		width: 70em;
+# 		margin: 0 auto;
+# 	}
+# </style>
+# </head>
+# <body>
+# <h1>Your app $app is currently under maintenance!</h1>
+# <p>This app has been put under maintenance by your administrator at $(date)</p>
+# <p>Please wait until the maintenance operation is done. This page will be reloaded as soon as your app will be back.</p>
 
-</body>
-</html>" > "/var/www/html/maintenance.$app.html"
+# </body>
+# </html>" > "/var/www/html/maintenance.$app.html"
 
-	# Create a new nginx config file to redirect all access to the app to the maintenance notice instead.
-	echo "# All request to the app will be redirected to ${path_url}_maintenance and fall on the maintenance notice
-rewrite ^${path_url}/(.*)$ ${path_url}_maintenance/? redirect;
-# Use another location, to not be in conflict with the original config file
-location ${path_url}_maintenance/ {
-alias /var/www/html/ ;
+# 	# Create a new nginx config file to redirect all access to the app to the maintenance notice instead.
+# 	echo "# All request to the app will be redirected to ${path}_maintenance and fall on the maintenance notice
+# rewrite ^${path}/(.*)$ ${path}_maintenance/? redirect;
+# # Use another location, to not be in conflict with the original config file
+# location ${path}_maintenance/ {
+# alias /var/www/html/ ;
 
-try_files maintenance.$app.html =503;
+# try_files maintenance.$app.html =503;
 
-# Include SSOWAT user panel.
-include conf.d/yunohost_panel.conf.inc;
-}" > "/etc/nginx/conf.d/$domain.d/maintenance.$app.conf"
+# # Include SSOWAT user panel.
+# include conf.d/yunohost_panel.conf.inc;
+# }" > "/etc/nginx/conf.d/$domain.d/maintenance.$app.conf"
 
-	# The current config file will redirect all requests to the root of the app.
-	# To keep the full path, we can use the following rewrite rule:
-	# 	rewrite ^${path_url}/(.*)$ ${path_url}_maintenance/\$1? redirect;
-	# The difference will be in the $1 at the end, which keep the following queries.
-	# But, if it works perfectly for a html request, there's an issue with any php files.
-	# This files are treated as simple files, and will be downloaded by the browser.
-	# Would be really be nice to be able to fix that issue. So that, when the page is reloaded after the maintenance, the user will be redirected to the real page he was.
+# 	# The current config file will redirect all requests to the root of the app.
+# 	# To keep the full path, we can use the following rewrite rule:
+# 	# 	rewrite ^${path}/(.*)$ ${path}_maintenance/\$1? redirect;
+# 	# The difference will be in the $1 at the end, which keep the following queries.
+# 	# But, if it works perfectly for a html request, there's an issue with any php files.
+# 	# This files are treated as simple files, and will be downloaded by the browser.
+# 	# Would be really be nice to be able to fix that issue. So that, when the page is reloaded after the maintenance, the user will be redirected to the real page he was.
 
-	systemctl reload nginx
-}
+# 	systemctl reload nginx
+# }
 
-ynh_maintenance_mode_OFF () {
-	# Load value of $path_url and $domain from the config if their not set
-	if [ -z $path_url ]; then
-		path_url=$(ynh_app_setting_get $app path)
-	fi
-	if [ -z $domain ]; then
-		domain=$(ynh_app_setting_get $app domain)
-	fi
+# ynh_maintenance_mode_OFF () {
+# 	# Load value of $path and $domain from the config if their not set
+# 	if [ -z $path ]; then
+# #REMOVEME? 		path=$(ynh_app_setting_get $app path)
+# 	fi
+# 	if [ -z $domain ]; then
+# #REMOVEME? 		domain=$(ynh_app_setting_get $app domain)
+# 	fi
 
-	# Rewrite the nginx config file to redirect from ${path_url}_maintenance to the real url of the app.
-	echo "rewrite ^${path_url}_maintenance/(.*)$ ${path_url}/\$1 redirect;" > "/etc/nginx/conf.d/$domain.d/maintenance.$app.conf"
-	systemctl reload nginx
+# 	# Rewrite the nginx config file to redirect from ${path}_maintenance to the real url of the app.
+# 	echo "rewrite ^${path}_maintenance/(.*)$ ${path}/\$1 redirect;" > "/etc/nginx/conf.d/$domain.d/maintenance.$app.conf"
+# 	systemctl reload nginx
 
-	# Sleep 4 seconds to let the browser reload the pages and redirect the user to the app.
-	sleep 4
+# 	# Sleep 4 seconds to let the browser reload the pages and redirect the user to the app.
+# 	sleep 4
 
-	# Then remove the temporary files used for the maintenance.
-	rm "/var/www/html/maintenance.$app.html"
-	rm "/etc/nginx/conf.d/$domain.d/maintenance.$app.conf"
+# 	# Then remove the temporary files used for the maintenance.
+# 	rm "/var/www/html/maintenance.$app.html"
+# 	rm "/etc/nginx/conf.d/$domain.d/maintenance.$app.conf"
 
-	systemctl reload nginx
-}
+# 	systemctl reload nginx
+# }
 
 #=================================================
 # EXPERIMENTAL HELPERS
@@ -141,8 +138,4 @@ ynh_maintenance_mode_OFF () {
 
 #=================================================
 # FUTURE OFFICIAL HELPERS
-#=================================================
-
-#=================================================
-# RUBY HELPER
 #=================================================
