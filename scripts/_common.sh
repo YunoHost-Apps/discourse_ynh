ca8a4993b1000723fb182c8008d1409415268a87
Félix Piédallu <felix@piedallu.me>
2024-05-20 20:47:12 +0200

    Use standard redis helpers

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -443,7 +443,7 @@ ynh_cleanup_ruby () {
             required_ruby_versions="${installed_app_ruby_version}\n${required_ruby_versions}"
         fi
     done
-    
+
     # Remove no more needed Ruby versions
     local installed_ruby_versions=$(rbenv versions --bare --skip-aliases | grep -Ev '/')
     for installed_ruby_version in $installed_ruby_versions
@@ -463,46 +463,4 @@ ynh_cleanup_ruby () {
         ynh_secure_remove --file="$rbenv_install_dir"
         ynh_secure_remove --file="/etc/profile.d/rbenv.sh"
     fi
-}
-
-#=================================================
-# REDIS HELPERS
-#=================================================
-
-# get the first available redis database
-#
-# usage: ynh_redis_get_free_db
-# | returns: the database number to use
-ynh_redis_get_free_db() {
-	local result max db
-	result=$(redis-cli INFO keyspace)
-
-	# get the num
-	max=$(cat /etc/redis/redis.conf | grep ^databases | grep -Eow "[0-9]+")
-
-	db=0
-	# default Debian setting is 15 databases
-	for i in $(seq 0 "$max")
-	do
-	 	if ! echo "$result" | grep -q "db$i"
-	 	then
-			db=$i
-	 		break 1
- 		fi
- 		db=-1
-	done
-
-	test "$db" -eq -1 && ynh_die --message="No available Redis databases..."
-
-	echo "$db"
-}
-
-# Create a master password and set up global settings
-# Please always call this script in install and restore scripts
-#
-# usage: ynh_redis_remove_db database
-# | arg: database - the database to erase
-ynh_redis_remove_db() {
-	local db=$1
-	redis-cli -n "$db" flushall
 }
