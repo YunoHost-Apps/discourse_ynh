19ad49c184628262705e3bd160a7b5357645c272
Félix Piédallu <felix.piedallu@semtech.com>
2024-08-29 12:52:07 +0200

    Tabs to spaces

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -17,56 +17,56 @@ libjemalloc="$(ldconfig -p | grep libjemalloc | awk 'END {print $NF}')"
 # Returns true if a swap partition is enabled, false otherwise
 # usage: is_swap_present
 is_swap_present() {
-  [ $(awk '/^SwapTotal:/{print $2}' /proc/meminfo)  -gt 0 ]
+    [ $(awk '/^SwapTotal:/{print $2}' /proc/meminfo)  -gt 0 ]
 }
 
 # Returns true if swappiness higher than 10
 # usage: is_swappiness_sufficient
 is_swappiness_sufficient() {
-  [ $(cat /proc/sys/vm/swappiness)  -gt 10 ]
+    [ $(cat /proc/sys/vm/swappiness)  -gt 10 ]
 }
 
 # Returns true if specified free memory is available (RAM + swap)
 # usage: is_memory_available MEMORY (in bytes)
 is_memory_available() {
-  local needed_memory=$1
-  local freemem="$(awk '/^MemAvailable:/{print $2}' /proc/meminfo)"
-  local freeswap="$(awk '/^SwapFree:/{print $2}' /proc/meminfo)"
-  [ $(($freemem+$freeswap)) -gt $needed_memory ]
+    local needed_memory=$1
+    local freemem="$(awk '/^MemAvailable:/{print $2}' /proc/meminfo)"
+    local freeswap="$(awk '/^SwapFree:/{print $2}' /proc/meminfo)"
+    [ $(($freemem+$freeswap)) -gt $needed_memory ]
 }
 
 # Checks discourse install memory requirements
 # terminates installation if requirements not met
 check_memory_requirements() {
-  if ! is_swap_present ; then
-    ynh_print_warn --message="You must have a swap partition in order to install and use this application"
-  elif ! is_swappiness_sufficient ; then
-    ynh_print_warn --message="Your swappiness must be higher than 10; please see https://en.wikipedia.org/wiki/Swappiness"
-  elif ! is_memory_available 1000000 ; then
-    ynh_print_warn --message="You must have a minimum of 1Gb available memory (RAM+swap) for the installation"
-  fi
+    if ! is_swap_present ; then
+        ynh_print_warn --message="You must have a swap partition in order to install and use this application"
+    elif ! is_swappiness_sufficient ; then
+        ynh_print_warn --message="Your swappiness must be higher than 10; please see https://en.wikipedia.org/wiki/Swappiness"
+    elif ! is_memory_available 1000000 ; then
+        ynh_print_warn --message="You must have a minimum of 1Gb available memory (RAM+swap) for the installation"
+    fi
 }
 # Checks discourse upgrade memory requirements
 # Less requirements as the software is already installed and running
 # terminates upgrade if requirements not met
 check_memory_requirements_upgrade() {
-  if ! is_memory_available 400000 ; then
-    ynh_die --message="You must have a minimum of 400Mb available memory (RAM+swap) for the upgrade"
-  fi
+    if ! is_memory_available 400000 ; then
+        ynh_die --message="You must have a minimum of 400Mb available memory (RAM+swap) for the upgrade"
+    fi
 }
 
 ynh_maintenance_mode_ON () {
-	# Create an html to serve as maintenance notice
-	echo "<!DOCTYPE html>
+    # Create an html to serve as maintenance notice
+    echo "<!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="refresh" content="3">
 <title>Your app $app is currently under maintenance!</title>
 <style>
-	body {
-		width: 70em;
-		margin: 0 auto;
-	}
+    body {
+        width: 70em;
+        margin: 0 auto;
+    }
 </style>
 </head>
 <body>
@@ -77,8 +77,8 @@ ynh_maintenance_mode_ON () {
 </body>
 </html>" > "/var/www/html/maintenance.$app.html"
 
-	# Create a new nginx config file to redirect all access to the app to the maintenance notice instead.
-	echo "# All request to the app will be redirected to ${path}_maintenance and fall on the maintenance notice
+    # Create a new nginx config file to redirect all access to the app to the maintenance notice instead.
+    echo "# All request to the app will be redirected to ${path}_maintenance and fall on the maintenance notice
 rewrite ^${path}/(.*)$ ${path}_maintenance/? redirect;
 # Use another location, to not be in conflict with the original config file
 location ${path}_maintenance/ {
@@ -90,30 +90,30 @@ try_files maintenance.$app.html =503;
 include conf.d/yunohost_panel.conf.inc;
 }" > "/etc/nginx/conf.d/$domain.d/maintenance.$app.conf"
 
-	# The current config file will redirect all requests to the root of the app.
-	# To keep the full path, we can use the following rewrite rule:
-	# 	rewrite ^${path}/(.*)$ ${path}_maintenance/\$1? redirect;
-	# The difference will be in the $1 at the end, which keep the following queries.
-	# But, if it works perfectly for a html request, there's an issue with any php files.
-	# This files are treated as simple files, and will be downloaded by the browser.
-	# Would be really be nice to be able to fix that issue. So that, when the page is reloaded after the maintenance, the user will be redirected to the real page he was.
+    # The current config file will redirect all requests to the root of the app.
+    # To keep the full path, we can use the following rewrite rule:
+    #     rewrite ^${path}/(.*)$ ${path}_maintenance/\$1? redirect;
+    # The difference will be in the $1 at the end, which keep the following queries.
+    # But, if it works perfectly for a html request, there's an issue with any php files.
+    # This files are treated as simple files, and will be downloaded by the browser.
+    # Would be really be nice to be able to fix that issue. So that, when the page is reloaded after the maintenance, the user will be redirected to the real page he was.
 
-	systemctl reload nginx
+    systemctl reload nginx
 }
 
 ynh_maintenance_mode_OFF () {
-	# Rewrite the nginx config file to redirect from ${path}_maintenance to the real url of the app.
-	echo "rewrite ^${path}_maintenance/(.*)$ ${path}/\$1 redirect;" > "/etc/nginx/conf.d/$domain.d/maintenance.$app.conf"
-	systemctl reload nginx
+    # Rewrite the nginx config file to redirect from ${path}_maintenance to the real url of the app.
+    echo "rewrite ^${path}_maintenance/(.*)$ ${path}/\$1 redirect;" > "/etc/nginx/conf.d/$domain.d/maintenance.$app.conf"
+    systemctl reload nginx
 
-	# Sleep 4 seconds to let the browser reload the pages and redirect the user to the app.
-	sleep 4
+    # Sleep 4 seconds to let the browser reload the pages and redirect the user to the app.
+    sleep 4
 
-	# Then remove the temporary files used for the maintenance.
-	rm "/var/www/html/maintenance.$app.html"
-	rm "/etc/nginx/conf.d/$domain.d/maintenance.$app.conf"
+    # Then remove the temporary files used for the maintenance.
+    rm "/var/www/html/maintenance.$app.html"
+    rm "/etc/nginx/conf.d/$domain.d/maintenance.$app.conf"
 
-	systemctl reload nginx
+    systemctl reload nginx
 }
 
 #=================================================
